8d36ecfbba4cdbae848491c8118be6b0079ca964
Lauramarie1612 <eaa25lmbj@students.eaaa.dk>
2025-11-05 11:07:14 +0100

    skole motervej

---
+++ js/animate-objects-on-road.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// Hent DOM-elementer
+
+const getRedcar = document.getElementById("redCar");
+const getPolicecar = document.getElementById("policeCar");
+const getBluecar = document.getElementById("blueCar");
+
+const sun = document.querySelector(".sun");
+const scene = document.querySelector(".scene");
+
+// opretter lydobjekter
+
+const soundRedCar = new Audio();
+soundRedCar.src = "../sound/red-car-horn.wav";
+
+const soundPoliceCar = new Audio();
+soundPoliceCar.src = "../sound/police-car-sound.wav";
+
+const soundBlueCar = new Audio();
+soundBlueCar.src = "../sound/blue-car-sound.wav";
+
+if (getRedcar) {
+  getRedcar.addEventListener("click", () => {
+    soundRedCar.play();
+  });
+}
+
+if (getPolicecar) {
+  getPolicecar.addEventListener("click", () => {
+    soundPoliceCar.play();
+  });
+}
+
+if (getBluecar) {
+  getBluecar.addEventListener("click", () => {
+    soundBlueCar.play();
+  });
+}
+
+if (sun && scene) {
+  sun.addEventListener("click", () => {
+    scene.classList.toggle("night");
+  });
+}
